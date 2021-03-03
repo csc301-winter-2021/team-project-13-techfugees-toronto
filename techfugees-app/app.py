1a2001201701dc7594752de46ed9826b2277fc63
andi <andi.fan@gmail.com>
2021-03-02 23:03:12 -0500

    add starter for member management

--- techfugees-app/app.py
+++ techfugees-app/app.py
@@ -1,5 +1,6 @@
-import sqlite3
-from flask import Flask, render_template
+import sqlite3, secrets
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 
 def get_db_connection():
     conn = sqlite3.connect('database.db')
@@ -7,7 +8,7 @@ def get_db_connection():
     return conn
 
 app = Flask(__name__)
-
+app.config['SECRET_KEY'] = secrets.token_hex(16)
 
 @app.route('/')
 def index():
@@ -15,3 +16,25 @@ def index():
     posts = conn.execute('SELECT * FROM posts').fetchall()
     conn.close()
     return render_template('index.html', posts=posts)
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}', 'success')
+        return redirect(url_for('index'))
+    return render_template('register.html', title="Create an Account", form=form)
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@email.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('index'))
+        else:
+            flash('Invalid Credentials!', 'danger')
+    return render_template('login.html', title='login', form=form)
+
+if __name__ == "__main__":
+    app.run(debug=True)
