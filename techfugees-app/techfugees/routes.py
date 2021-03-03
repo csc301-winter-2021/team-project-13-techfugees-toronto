0eaf7fca7c890bb54681ebca9f2c3f66f70061e1
andi <andi.fan@gmail.com>
2021-03-03 16:26:18 -0500

    add crud functionality to listing prototype

--- techfugees-app/techfugees/routes.py
+++ techfugees-app/techfugees/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, url_for, flash, redirect, request
+from flask import render_template, url_for, flash, redirect, request, abort
 from techfugees import app, db, bcrypt
 from techfugees.forms import RegistrationForm, LoginForm, UpdateAccountForm, NewListingForm
 from techfugees.models import User, Post
@@ -74,3 +74,38 @@ def new_rental_posting():
         flash('Listing Added!', 'success')
         return redirect(url_for('index'))
     return render_template('create_post.html', title='Add New Listing', form=form)
+
+@app.route('/post/<int:post_id>', methods=['GET','POST'])
+def listing(post_id):
+    listing = Post.query.get_or_404(post_id)
+    return render_template('listing.html', title=listing.title, post=listing)
+
+@app.route('/post/<int:post_id>/update', methods=['GET','POST'])
+@login_required
+def update_listing(post_id):
+    listing = Post.query.get_or_404(post_id)
+    if listing.author != current_user:
+        abort(403)
+    form = NewListingForm()
+    if form.validate_on_submit():
+        listing.title = form.title.data #SQLalchemy convention, post refers to Post class, and is lowercase here
+        listing.content =form.content.data
+        db.session.commit()
+        flash('Post updated', 'success')
+        return redirect(url_for('listing', post_id=listing.id))
+    elif request.method == 'GET':
+        form.title.data = listing.title
+        form.content.data = listing.content
+
+    return render_template('create_post.html', title='Update Post', form=form)
+
+@app.route("/post/<int:post_id>/delete", methods=['POST'])
+@login_required
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    db.session.delete(post)
+    db.session.commit()
+    flash('Your post has been deleted!', 'success')
+    return redirect(url_for('index'))
