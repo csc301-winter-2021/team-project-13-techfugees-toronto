84d0c8dd1b8ba55434683318af9db6a083b312b5
andi <andi.fan@gmail.com>
2021-03-03 20:57:59 -0500

    wip landlord class

--- techfugees-app/techfugees/models.py
+++ techfugees-app/techfugees/models.py
@@ -1,6 +1,7 @@
 from techfugees import db, login_manager, app
 from datetime import datetime
 from flask_login import UserMixin #adds attributes lik authenticated, active, anon
+from flask_sqlalchemy import Model, SQLAlchemy
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -14,25 +15,22 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(120), unique=True, nullable=False)
     image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
     password = db.Column(db.String(60), nullable=False)
+    posts = db.relationship('Post', backref='author', lazy=True)
 
     def __repr__(self):
         return f"User('{self.username}, {self.email}, {self.image_file}')"
 
-class Landlord(db.Model, UserMixin):
-    id = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
-    user = db.relationship('User', backref='landlord', lazy=True)
-    username = db.Column(db.String(20), db.ForeignKey('user.username'), unique=True, nullable=False)
+class Landlord(User):
     credit_check = db.Column(db.Boolean, default=False)
     first_last = db.Column(db.Boolean, default=False)
-    posts = db.relationship('Post', backref='author', lazy=True)
-
+    
     def __repr__(self):
         return f"User('{self.username}')"
 
 #template class, eventually change to fit rental postings
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('landlord.id'), nullable=False, unique=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False, unique=True)
     title = db.Column(db.String(20), nullable=False)
     content = db.Column(db.String(120), nullable=False)
     date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
