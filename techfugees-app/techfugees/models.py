0ea58bf901289374231b9e4f2238a60081ec2c16
MubaiGao <Gary19990618@gmail.com>
2021-03-05 13:24:31 -0800

    add new col in models

--- techfugees-app/techfugees/models.py
+++ techfugees-app/techfugees/models.py
@@ -24,8 +24,25 @@ class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(20), nullable=False)
     content = db.Column(db.String(120), nullable=False)
+    address = db.Column(db.String(120), nullable=False)
+    city = db.Column(db.String(20), nullable=False)
+    pet = db.Column(db.Boolean, default=False)
+    smoking = db.Column(db.Boolean, default=False)
+    balcony = db.Column(db.Boolean, default=False)
+    air_conditioning = db.Column(db.Boolean, default=False)
+    stove_oven = db.Column(db.Boolean, default=False)
+    washer = db.Column(db.Boolean, default=False)
+    dryer = db.Column(db.Boolean, default=False)
+    dishwasher = db.Column(db.Boolean, default=False)
+    microwave = db.Column(db.Boolean, default=False)
+    cable = db.Column(db.Boolean, default=False)
+    water = db.Column(db.Boolean, default=False)
+    electricity = db.Column(db.Boolean, default=False)
+    num_bedrooms = db.Column(db.Integer, nullable=False)
+    num_bathrooms = db.Column(db.Integer, nullable=False)
+    type_of_building = db.Column(db.String(20), nullable=False)
     date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
     def __repr__(self):
-        return f"Post('{self.title}, {self.id}, {self.date_posted}')"
+        return f"Post('{self.title}, {self.id}, {self.date_posted}, {self.address}, {self.city}, {self.pet}, {self.smoking}, {self.balcony}, {self.air_conditioning}, {self.stove_oven}, {self.washer}, {self.dryer}, {self.dishwasher}, {self.microwave}, {self.cable}, {self.water}, {self.electricity}, {self.num_bathrooms}, {self.num_bedrooms}, {self.type_of_building},')"
--- techfugees-app/techfugees/posts/routes.py
+++ techfugees-app/techfugees/posts/routes.py
@@ -7,24 +7,46 @@ from techfugees.posts.forms import NewListingForm
 
 posts = Blueprint('posts', __name__)
 
-@posts.route('/post/new', methods=['GET','POST'])
+
+@posts.route('/post/new', methods=['GET', 'POST'])
 @login_required
 def new_rental_posting():
     form = NewListingForm()
     if form.validate_on_submit():
-        listing = Post(title=form.title.data, content=form.content.data, author=current_user)
+        listing = Post(title=form.title.data,
+                       address=form.address.data,
+                       city=form.city.data,
+                       pet=form.pet.data,
+                       smoking=form.smoking.data,
+                       balcony=form.balcony.data,
+                       air_conditioning=form.air_conditioning.data,
+                       stove_oven=form.stove_oven.data,
+                       washer=form.washer.data,
+                       dryer=form.dryer.data,
+                       dishwasher=form.dishwasher.data,
+                       microwave=form.microwave.data,
+                       cable=form.cable.data,
+                       water=form.water.data,
+                       electricity=form.electricity.data,
+                       num_bathrooms=form.num_bathrooms.data,
+                       num_bedrooms=form.num_bedrooms.data,
+                       type_of_building=form.type_of_building.data,
+                       content=form.content.data,
+                       author=current_user)
         db.session.add(listing)
         db.session.commit()
         flash('Listing Added!', 'success')
         return redirect(url_for('main.index'))
     return render_template('create_post.html', title='Add New Listing', form=form)
 
-@posts.route('/post/<int:post_id>', methods=['GET','POST'])
+
+@posts.route('/post/<int:post_id>', methods=['GET', 'POST'])
 def listing(post_id):
     listing = Post.query.get_or_404(post_id)
     return render_template('listing.html', title=listing.title, post=listing)
 
-@posts.route('/post/<int:post_id>/update', methods=['GET','POST'])
+
+@posts.route('/post/<int:post_id>/update', methods=['GET', 'POST'])
 @login_required
 def update_listing(post_id):
     listing = Post.query.get_or_404(post_id)
@@ -32,8 +54,9 @@ def update_listing(post_id):
         abort(403)
     form = NewListingForm()
     if form.validate_on_submit():
-        listing.title = form.title.data #SQLalchemy convention, post refers to Post class, and is lowercase here
-        listing.content =form.content.data
+        # SQLalchemy convention, post refers to Post class, and is lowercase here
+        listing.title = form.title.data
+        listing.content = form.content.data
         db.session.commit()
         flash('Post updated', 'success')
         return redirect(url_for('posts.listing', post_id=listing.id))
@@ -42,6 +65,7 @@ def update_listing(post_id):
         form.content.data = listing.content
 
     return render_template('create_post.html', title='Update Post', form=form)
+
 
 @posts.route("/post/<int:post_id>/delete", methods=['POST'])
 @login_required
